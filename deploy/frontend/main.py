9d20e19aca5085301e3e2862381266000c706a6b
chen2mg <chen2mg@mail.uc.edu>
2023-10-09 17:03:09 -0400

    updates

--- deploy/frontend/main.py
+++ deploy/frontend/main.py
@@ -2,6 +2,7 @@ from multiapp import MultiApp
 from app1 import style_gan
 from app2 import demo_app
 from app3 import image_seg
+from will_projects import object
 
 backend = 'http://backend:8080'
 
@@ -9,10 +10,11 @@ home = MultiApp(backend)
 
 home.add_category('Welcome', {'Welcome Page':demo_app.app})
 
-home.add_category('object detection', {'Style Transfer':style_gan.app, 
+home.add_category('demo apps1', {'Style Transfer':style_gan.app, 
                                     'image segmentation': image_seg.app})
 
-home.add_category('human pose detection', {'image segmentation':image_seg.app})
+home.add_category('demo apps2', {'image segmentation':image_seg.app,
+                                 'object detection': object.app})
 
 
 if __name__ == "__main__":
---
+++ deploy/frontend/will_projects/object.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+def app(backend):
+    st.header('Hellow world')
